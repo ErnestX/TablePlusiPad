fece532b609328e744a18a5a17b12f212370fbe8
ErnestX <ernestxiangjlnq2011@gmail.com>
2015-06-22 15:44:15 -0700

    Revert "commit before switching branch"
    This reverts commit cebfe63fd9f7ed172e7983e7b5f38c2b363e1497.

--- TablePlusiPad/Vector3D.h
+++ TablePlusiPad/Vector3D.h
@@ -14,11 +14,8 @@
 
 - (instancetype)initWithX:(float)xv Y:(float)yv Z:(float)zv;
 - (Vector3D*)plus:(Vector3D*)v;
-- (void)setModule:(float)m;
 - (float)getModule;
 - (Vector3D*)calcMultiplyByConst:(float)c;
 - (Vector3D*)normalize;
-- (float)calcDotProd:(Vector3D*)v;
-- (float)calcAngleToPlaneWithNormal:(Vector3D*)n;
 
 @end
--- TablePlusiPad/Vector3D.m
+++ TablePlusiPad/Vector3D.m
@@ -67,14 +67,4 @@
     return [self calcDivideByConst:[self getModule]];
 }
 
-- (float)calcDotProd:(Vector3D*)v
-{
-    return self.x * v.x + self.y * v.y + self.z * v.z;
-}
-
-- (float)calcAngleToPlaneWithNormal:(Vector3D*)n
-{
-    return asinf(fabsf([n calcDotProd:self]) / ([n getModule]*[self getModule]));
-}
-
 @end
